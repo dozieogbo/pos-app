23e9f0a95459fa5e416a039b7c40aba412c9010f
Chidozie Ogbo <chidozieogbo@outlook.com>
2018-05-25 10:56:30 +0100

    updated migrations

--- database/migrations/2018_04_24_101635_create_supplies_table.php
+++ database/migrations/2018_04_24_101635_create_supplies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateArrivalsTable extends Migration
+class CreateSuppliesTable extends Migration
 {
     /**
      * Run the migrations.
